67ff901db86c61fc87efc4c952719ac05010d284
Paul Evans <decretist@gmail.com>
2015-01-16 17:12:45 -0500

    canons/testbed.py now working for D.1-101.

--- canons/testbed.py
+++ canons/testbed.py
@@ -10,9 +10,9 @@ def main():
     f = open('./edF.txt', 'r')
     file = f.read()
     toc = open('./toc.txt', 'r')
-    dictionary = {}
-    dictionary_r1 = {}
-    dictionary_r2 = {}
+    dictionary_Fr = {} # Friedberg
+    dictionary_1r = {} # first recension
+    dictionary_2r = {} # second recension
     # (?<=...) positive lookbehind assertion.
     canons = re.findall('(?:\<T T\>|(?<=\<T T\>))(.*?)'    # canon starts with text (<T T>) tag.
         '(?:'                   # non-capturing group.
@@ -36,18 +36,38 @@ def main():
         canon = re.sub('^\s+', '', canon) # remove leading whitespace characters
         canon = re.sub('\s+$', '', canon) # remove trailing whitespace characters
         key = toc.readline().rstrip()
-        if key in dictionary:
+        if key in dictionary_Fr:
         # if there's already a dictionary entry with this key, merge the entries
             # print('duplicate key: ' + key, file=sys.stderr)
-            canon = dictionary[key] + ' ' + canon
-        dictionary[key] = canon
+            canon = dictionary_Fr[key] + ' ' + canon
+        dictionary_Fr[key] = canon
+
+    keys = tuple(open('./toc_1r.txt', 'r'))
+    for key in keys:
+        key = key.rstrip()
+        dictionary_1r[key] = dictionary_Fr[key].lower() # copy canon from Friedberg dictionary into first-recension dictionary
+        # print(key + '\n' + dictionary_1r[key] + '\n', file=sys.stderr)
+        outfilename_r1 = './tmp/auto/1r/' + key + '.txt'
+        f = open(outfilename_r1, 'w')
+        f.write(dictionary_1r[key] + '\n')
+        f.close
+
+    keys = tuple(open('./toc_2r.txt', 'r'))
+    for key in keys:
+        key = key.rstrip()
+        dictionary_2r[key] = dictionary_Fr[key] # copy canon from Friedberg dictionary into second-recension dictionary
+        # print(key + '\n' + dictionary_2r[key] + '\n', file=sys.stderr)
+        outfilename_2r = './tmp/auto/2r_pre/' + key + '.txt'
+        f = open(outfilename_2r, 'w')
+        f.write(dictionary_2r[key] + '\n')
+        f.close
 
     keysandpatterns = [
-        # {'key': 'D.5 c.4', 'pattern': '(Ad eius uero.*?qui gignitur ablactetur\.).*?(Si autem filios.*?est, ut esuriamus\.)'},
+        {'key': 'D.5 c.4', 'pattern': '(Ad eius uero.*?qui gignitur ablactetur\.)(.*?)(Si autem filios.*?est, ut esuriamus\.)'},
         {'key': 'D.9 c.1', 'pattern': '(Quicumque legibus imperatorum.*?acquirit grande premium\.)'},
-        # {'key': 'D.10 c.1', 'pattern': '(Lege imperatorum non.*?iura ecclesiastica dissolui\.).*?(Non quod imperatorum.*?inferre preiudicium asseramus\.)'},
+        {'key': 'D.10 c.1', 'pattern': '(Lege imperatorum non.*?iura ecclesiastica dissolui\.)(.*?)(Non quod imperatorum.*?inferre preiudicium asseramus\.)'},
         {'key': 'D.17 c.1', 'pattern': '(Sinodum episcoporum absque.*?potestis regulariter facere\.)'},
-        # {'key': 'D.19 c.1', 'pattern': '(Si Romanorum Pontificum.*?sibi ueniam denegari\.).*?(Dicendo uero: "omnia.*?Gelasium, mandasse probauimus\.)'},
+        {'key': 'D.19 c.1', 'pattern': '(Si Romanorum Pontificum.*?sibi ueniam denegari\.)(.*?)(Dicendo uero: "omnia.*?Gelasium, mandasse probauimus\.)'},
         {'key': 'D.32 c.6', 'pattern': '(Preter hoc autem.*?ecclesiae communione separentur\.)'},
         {'key': 'D.38 c.4', 'pattern': '(Nulli sacerdotum liceat.*?Patrum regulis obuiare\.)'},
         {'key': 'D.48 c.2', 'pattern': '(Sicut neophitus dicebatur,.*?abrupta querit ascensum\.)'},
@@ -60,7 +80,7 @@ def main():
         {'key': 'D.62 c.1', 'pattern': '(Nulla ratio sinit,.*?a plebibus expetiti,)'},
         {'key': 'D.62 c.2', 'pattern': '(Docendus est populus, non sequendus,)'},
         {'key': 'D.67 c.2', 'pattern': '(Episcopus sacerdotibus ac ministris solus honorem dare potest,)'},
-        # {'key': 'D.68 c.4', 'pattern': '(Quamuis corepiscopis et.*?cuilibet epistolas mittere\.).*?(Quoniam, quamquam consecrationem.*?apicem non habent\.)'},
+        {'key': 'D.68 c.4', 'pattern': '(Quamuis corepiscopis et.*?cuilibet epistolas mittere\.)(.*?)(Quoniam, quamquam consecrationem.*?apicem non habent\.)'},
         {'key': 'D.74 c.2', 'pattern': '(sicut iustum est,.*?ministerio deiciatur iniuste\.)'},
         {'key': 'D.89 c.5', 'pattern': '(cauendum est a.*?amministrent, quam accusent\.)'},
         {'key': 'D.93 c.13', 'pattern': '(Diaconos propriam constituimus.*?facere plerumque conceditur\.)'},
@@ -73,25 +93,32 @@ def main():
     for i in range (len(keysandpatterns)):
         key = keysandpatterns[i]['key']
         pattern = keysandpatterns[i]['pattern']
-        dictionary_r2[key] = fixString(re.sub(pattern, '', dictionary[key])) # text added in second recension
-        # print(dictionary_r2[key], file=sys.stderr)
+        result = re.search(pattern, dictionary_Fr[key])
+        if result:
+            if len(result.groups()) == 1:
+                dictionary_1r[key] = fixString(result.group(1))
+                dictionary_2r[key] = fixString(re.sub(pattern, '', dictionary_Fr[key]))
+            elif len(result.groups()) == 3:
+                dictionary_1r[key] = fixString(result.group(1)) + ' ' + fixString(result.group(3))
+                dictionary_2r[key] = fixString(result.group(2))
+        else:
+            print('no match: ' + key + '\n' + dictionary_Fr[key], file=sys.stderr)
 
-    keys = tuple(open('./toc_test.txt', 'r')) # uniq toc.txt > dedup.txt
+    keys = tuple(open('./toc_edits.txt', 'r')) # uniq toc.txt > dedup.txt
     for key in keys:
         key = key.rstrip()
         #
-        print(key + '\n' + dictionary_r2[key] + '\n')
-        #
-        outfilename = './tmp/auto/2r_pre/' + key + '.txt'
-        f = open(outfilename, 'w')
-        f.write(dictionary_r2[key] + '\n')
+        print(key + '\n' + dictionary_1r[key] + '\n', file=sys.stderr)
+        outfilename_1r = './tmp/auto/1r/' + key + '.txt'
+        f = open(outfilename_1r, 'w')
+        f.write(dictionary_1r[key] + '\n')
         f.close
         #
-        # Generate files for hand-editing. Only do this once, or you'll overwrite your work!
-        # outfilename = './tmp/hand/2r/' + key + '.txt'
-        # f = open(outfilename, 'w')
-        # f.write(dictionary[key] + '\n')
-        # f.close
+        print(key + '\n' + dictionary_2r[key] + '\n', file=sys.stderr)
+        outfilename_2r = './tmp/auto/2r_pre/' + key + '.txt'
+        f = open(outfilename_2r, 'w')
+        f.write(dictionary_2r[key] + '\n')
+        f.close
 
 def fixString(string):
     string = re.sub('\s+', ' ', string) # 2r
