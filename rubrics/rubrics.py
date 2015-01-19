4a4808084d3b43a5b848ad7c3640451683dc94ef
Paul Evans <decretist@gmail.com>
2015-01-19 12:26:13 -0500

    rubrics/rubrics.py now produces first- and second-recension output files.

--- rubrics/rubrics.py
+++ rubrics/rubrics.py
@@ -8,21 +8,43 @@ import sys
 def main():
     f = open('./edF.txt', 'r')
     file = f.read()
-    dictionary = {}
+    toc = open('./toc.txt', 'r')
+    dictionary_1r = {}
+    dictionary_2r = {}
+    dictionary_Fr = {}
     # (?<=...) positive lookbehind assertion.
-    rubrics = re.findall('(?:\<T R\>|(?<=\<T R\>))(.*?)'    # rubric starts with rubric tag.
+    rubrics = re.findall('(?:\<T R\>|(?<=\<T R\>))(.*?)'    # rubric starts with rubric (<T R>) tag.
         '(?:'                   # non-capturing group.
             '\<T [IPT]\>'       # rubric ends with inscription, dictum (post), or text tag.
         ')', file, re.S)        # re.S (re.DOTALL) makes '.' special character match any character including newline.
     print('expected 3422 rubrics, found ' + str(len(rubrics)) + ' rubrics', file=sys.stderr)
     for rubric in rubrics:
-        rubric = re.sub('\<P 1\> \-\[PALEA\.\+', '', rubric)    # remove Palea tags.
+        rubric = re.sub('\<P 1\> \-\[PALEA\.\+', '', rubric) # remove Palea tag.
         rubric = re.sub(re.compile('\-\[.*?\]\+', re.S), '', rubric)
         rubric = re.sub('\-.*?\+', '', rubric)
         rubric = re.sub('\s+', ' ', rubric)
         rubric = re.sub('^\s+', '', rubric) # remove leading whitespace characters
         rubric = re.sub('\s+$', '', rubric) # remove trailing whitespace characters
-        print(rubric)
+        key = toc.readline().rstrip()
+        dictionary_Fr[key] = rubric
+    #
+    keys = tuple(open('./toc_1r.txt', 'r'))
+    outfilename_r1 = './tmp/Gratian1.txt'
+    f = open(outfilename_r1, 'w')
+    for key in keys:
+        key = key.rstrip()
+        dictionary_1r[key] = dictionary_Fr[key]
+        f.write(dictionary_1r[key] + '\n')
+    f.close
+    #
+    keys = tuple(open('./toc_2r.txt', 'r'))
+    outfilename_2r = './tmp/Gratian2.txt'
+    f = open(outfilename_2r, 'w')
+    for key in keys:
+        key = key.rstrip()
+        dictionary_2r[key] = dictionary_Fr[key]
+        f.write(dictionary_2r[key] + '\n')
+    f.close
 
 if __name__ == '__main__':
     main()
---
+++ rubrics/toc.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+#
+# Paul Evans (10evans@cardinalmail.cua.edu)
+#
+import re
+def main():
+    seite = ''
+    pars = ''
+    number = ''
+    # distinctio = ''
+    # causa = ''
+    quaestio = ''
+    canon = '0'
+    f = open('./bones.txt', 'r')
+    for line in f:
+        m10 = re.match(r'\<1 C>', line) # Causa
+        m11 = re.match(r'\<1 D>', line) # Distinctio
+        m12 = re.match(r'\<1 DC>', line) # De Consecratione
+        m13 = re.match(r'\<1 DP>', line) # De Penitentia
+        m14 = re.match(r'\<2 (\d{1,3})\>', line)
+        m15 = re.match(r'\<3 (\d{1,2})\>', line)
+        m16 = re.match(r'\<4 (\d{1,3})\>', line)
+        # m20 = re.search('\<P 0\>', line) # Palea end
+        # m21 = re.match('\<P 1\>', line) # Palea start
+        m22 = re.match(r'\<S (\d{1,4})\>', line) 
+        m23 = re.match(r'\<T A\>', line) # Dictum ante
+        # m24 = re.match(r'\<T I\>', line) # Inscriptio
+        m25 = re.match(r'\<T P\>', line) # Dictum post
+        m26 = re.match(r'\<T Q\>', line) # Quaestio
+        m27 = re.match(r'\<T R\>', line) # Rubrik
+        m28 = re.match(r'\<T T\>', line) # Text des Kapitels
+        m30 = re.search(r'\<.*?\>', line)
+
+        if m10:
+            canon = '0'
+            pars = 'C.'
+            pass
+        elif m11:
+            canon = '0'
+            quaestio = ''
+            pars = 'D.'
+            pass
+        elif m12: # De Consecratione
+            canon = '0'
+            quaestio = ''
+            pars = 'de Cons. D.'
+            pass
+        elif m13: # De Penitentia
+            canon = '0'
+            quaestio = ''
+            pars = 'de Pen. D.'
+            pass
+        elif m14:
+            number = str(m14.group(1)) + ' '
+            pass
+        elif m15:
+            canon = '0'
+            quaestio = 'q.' + str(m15.group(1)) + ' '
+            pass
+        elif m16: # <4 \d{1,3}>
+            canon = m16.group(1)
+            # print(pars + number + quaestio + 'c.' + canon)
+        elif m22:
+            seite = str(m22.group(1)) + ': '
+            pass
+        elif m23: # <T A>
+            # print(pars + number + quaestio + 'd.a.c.' + str(int(canon) + 1))
+            pass
+        elif m25: # <T P>
+            # print(pars + number + quaestio + 'd.p.c.' + canon)
+            pass
+        elif m26: # <T Q>
+            # print(seite + pars + number + 'd.init')
+            pass
+        elif m27: # <T R>
+            print(pars + number + quaestio + 'c.' + canon)
+            pass
+        elif m28: # <T T>
+            # print(pars + number + quaestio + 'c.' + canon)
+            pass
+
+if __name__ == '__main__':
+    main()
