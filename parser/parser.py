1940edbdfa0c272cec4a8f1c94a63c9ca118ff38
Paul Evans <decretist@gmail.com>
2015-01-24 13:13:28 -0500

    parser/parser.py can now extract distinction number and d.a.c.1 text.

--- parser/parser.py
+++ parser/parser.py
@@ -15,18 +15,20 @@ def main():
     file = re.sub('\<L \d{1,2}\>', '', file) # remove line number tags
     file = re.sub('\<P 1\>|\<P 0\>', '', file) # remove Palea tags
     file = re.sub('\s+', ' ', file) # remove multiple whitespace characters
-    file = re.sub('\s+$', '', file) # remove trailing whitespace characters
-    parse_all(file)
+    parse_all(file.strip(' '))
 # parse distinctions
 def parse_all(text):
     distinctions = re.findall('(?:\<1 D\>)(.*?)(?=\<1 D\>|$)', text)
     for distinction in distinctions:
-        parse_sub(distinction)
+        distinction = distinction.strip(' ')
+        m = re.match('\<2 (\d{1,3})\> \<T A\> (.*?) (?=\<4 1\>)', distinction)
+        if m:
+            print('D.' + m.group(1) + ' d.a.c.1: ' + m.group(2))
 # parse canons
 def parse_sub(text):
     canons = re.findall('(\<4 \d{1,2}\>.*?)(?=\<4 \d{1,2}\>|$)', text)
     for canon in canons:
-        print(canon)
+        print(canon.strip(' '))
 
 if __name__ == '__main__':
     main()
