877f434e12067e47f179fd6184847e27f89f8873
Paul Evans <decretist@gmail.com>
2015-01-26 20:57:55 -0500

    parser/parser.py can now extract text associated with <T ?> tags within canons.

--- parser/parser.py
+++ parser/parser.py
@@ -5,6 +5,7 @@
 #
 from __future__ import print_function
 import re
+import sys
 canon_number = ''
 distinction_number = ''
 table_of_contents = []
@@ -14,6 +15,7 @@ def main():
     file = f.read()
     f.close()
     parse_distinctions(preprocess(file))
+    print(table_of_contents)
 
 def parse_distinctions(text):
     global distinction_number
@@ -22,9 +24,11 @@ def parse_distinctions(text):
         distinction = distinction.strip(' ')
         m = re.match('\<2 (\d{1,3})\> \<T A\> (.*?) (?=\<4 1\>)', distinction)
         distinction_number = 'D.' + m.group(1)
-        # key = distinction_number + ' d.a.c.1'
-        # table_of_contents.append(key)
-        # dictionary[key] = m.group(2)
+        #
+        key = distinction_number + ' d.a.c.1'
+        table_of_contents.append(key)
+        dictionary[key] = m.group(2)
+        #
         parse_canons(distinction)
 
 def parse_canons(text):
@@ -34,14 +38,38 @@ def parse_canons(text):
         canon = canon.strip(' ')
         m = re.match('\<4 (\d{1,2})\>', canon)
         canon_number = 'c.' + m.group(1)
-        print(distinction_number + ' ' + canon_number + ':')
         parse_parts(canon)
 
 def parse_parts(text):
+    T_P_flag = False
+    T_T_flag = False
     parts = re.findall('(\<T [AIPRT]\>.*?)(?=\<T [AIPRT]\>|$)', text)
     for part in parts:
         part = part.strip(' ')
-        print(part)
+        m = re.match('(\<T [AIPRT]\>) (.+)', part)
+        tag = m.group(1)
+        if tag == '<T A>':
+            key = distinction_number + ' d.a.' + canon_number
+            print('Warning: unexpected <T A> tag: ' + key, file=sys.stderr)
+        if tag == '<T I>' or tag == '<T R>':
+            pass
+        if tag == '<T P>':
+            key = distinction_number + ' d.p.' + canon_number
+            if T_P_flag:
+                print('Warning: multiple <T P> tags: ' + key, file=sys.stderr)
+            else:
+                table_of_contents.append(key)
+                dictionary[key] = m.group(2)
+                T_P_flag = True
+        if tag == '<T T>':
+            key = distinction_number + ' ' + canon_number
+            if T_T_flag:
+                print('Warning: multiple <T T> tags: ' + key, file=sys.stderr)
+                pass
+            else:
+                table_of_contents.append(key)
+                dictionary[key] = m.group(2)
+                T_T_flag = True
 
 def preprocess(text):
     text = re.sub(re.compile('\-.*?\+', re.S), '', text) # remove comments
