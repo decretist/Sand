45824f60df8a6127cce10782a942e86091e74a1f
Paul Evans <decretist@gmail.com>
2015-01-28 21:10:38 -0500

    Extended parser.py to handle part 2 (cases).

--- parser/parser.py
+++ parser/parser.py
@@ -7,15 +7,44 @@ from __future__ import print_function
 import re
 import sys
 canon_number = ''
+case_number = ''
 distinction_number = ''
+question_number = ''
 table_of_contents = []
 dictionary = {}
 def main():
-    f = open('./edF.txt', 'r')
-    file = f.read()
-    f.close()
-    parse_distinctions(preprocess(file))
-    print(table_of_contents)
+    # parse_distinctions(preprocess(open('./part1.txt', 'r').read()))
+    parse_cases(preprocess(open('./part2.txt', 'r').read()))
+    # print(table_of_contents)
+
+def parse_cases(text):
+    global case_number
+    cases = re.findall('(?:\<1 C\>)(.*?)(?=\<1 C\>|$)', text)
+    for case in cases:
+        case = case.strip(' ')
+        m = re.match('\<2 (\d{1,2})\><T Q> (.*?) (?=\<3 1\>)', case)
+        case_number = 'C.' + m.group(1)
+        #
+        key = case_number + ' d.init.'
+        table_of_contents.append(key)
+        dictionary[key] = m.group(2)
+        #
+        parse_questions(case)
+
+def parse_questions(text):
+    global question_number
+    questions = re.findall('(\<3 \d{1,2}\>.*?)(?=\<3 \d{1,2}\>|$)', text)
+    for question in questions:
+        question = question.strip(' ')
+        m = re.match('\<3 (\d{1,2})\> \<T A\> (.*?) (?=\<4 1\>)', question)
+        if m:
+            question_number = 'q.' + m.group(1)
+            key = case_number + ' ' + question_number + ' d.a.c.1'
+            table_of_contents.append(key)
+            dictionary[key] = m.group(2)
+            parse_canons(question)
+        else:
+            continue
 
 def parse_distinctions(text):
     global distinction_number
@@ -49,12 +78,14 @@ def parse_parts(text):
         m = re.match('(\<T [AIPRT]\>) (.+)', part)
         tag = m.group(1)
         if tag == '<T A>':
-            key = distinction_number + ' d.a.' + canon_number
+            # key = distinction_number + ' d.a.' + canon_number
+            key = case_number + ' ' + question_number  + ' d.a.' + canon_number
             print('Warning: unexpected <T A> tag: ' + key, file=sys.stderr)
         if tag == '<T I>' or tag == '<T R>':
             pass
         if tag == '<T P>':
-            key = distinction_number + ' d.p.' + canon_number
+            # key = distinction_number + ' d.p.' + canon_number
+            key = case_number + ' ' + question_number + ' d.p.' + canon_number
             if T_P_flag:
                 print('Warning: multiple <T P> tags: ' + key, file=sys.stderr)
             else:
@@ -62,7 +93,8 @@ def parse_parts(text):
                 dictionary[key] = m.group(2)
                 T_P_flag = True
         if tag == '<T T>':
-            key = distinction_number + ' ' + canon_number
+            # key = distinction_number + ' ' + canon_number
+            key = case_number + ' ' + question_number + ' ' + canon_number
             if T_T_flag:
                 print('Warning: multiple <T T> tags: ' + key, file=sys.stderr)
                 pass
