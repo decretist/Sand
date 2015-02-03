e5a7d2fb54c9050e071b71c6dcf8e8af09228375
Paul Evans <decretist@gmail.com>
2015-02-02 19:53:37 -0500

    Phase 1 of refactoring to dictionary of list of tuples data structure.

--- parser/parser.py
+++ parser/parser.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 #
 # Paul Evans (10evans@cardinalmail.cua.edu)
-# 23-31 January 2015
+# 23 January 2015 -
+# 2 February 2015
 #
 from __future__ import print_function
 import re
@@ -18,9 +19,9 @@ def parse_distinctions(text):
     distinctions = re.findall('(?:\<1 D\>)(.*?)(?=\<1 D\>|$)', text)
     for distinction in distinctions:
         distinction = distinction.strip(' ')
-        m = re.match('\<2 (\d{1,3})\> \<T A\> (.*?) (?=\<4 1\>)', distinction)
+        m = re.match('\<2 (\d{1,3})\> (\<T A\>) (.*?) (?=\<4 1\>)', distinction)
         citation_stack.append('D.' + m.group(1))
-        add_to_dictionary('d.a.c.1', m.group(2))
+        add_to_dictionary('d.a.c.1', (m.group(2), m.group(3)))
         parse_canons(distinction)
         citation_stack.pop()
 
@@ -28,9 +29,9 @@ def parse_cases(text):
     cases = re.findall('(?:\<1 C\>)(.*?)(?=\<1 C\>|$)', text)
     for case in cases:
         case = case.strip(' ')
-        m = re.match('\<2 (\d{1,2})\><T Q> (.*?) (?=\<3 1\>)', case)
+        m = re.match('\<2 (\d{1,2})\>(\<T Q\>) (.*?) (?=\<3 1\>)', case)
         citation_stack.append('C.' + m.group(1))
-        add_to_dictionary('d.init.', m.group(2))
+        add_to_dictionary('d.init.', (m.group(2), m.group(3)))
         parse_questions(case)
         citation_stack.pop()
 
@@ -38,10 +39,10 @@ def parse_questions(text):
     questions = re.findall('(\<3 \d{1,2}\>.*?)(?=\<3 \d{1,2}\>|$)', text)
     for question in questions:
         question = question.strip(' ')
-        m = re.match('\<3 (\d{1,2})\> \<T A\> (.*?) (?=\<4 1\>)', question)
+        m = re.match('\<3 (\d{1,2})\> (\<T A\>) (.*?) (?=\<4 1\>)', question)
         if m:
             citation_stack.append('q.' + m.group(1))
-            add_to_dictionary('d.a.c.1', m.group(2))
+            add_to_dictionary('d.a.c.1', (m.group(2), m.group(3)))
             parse_canons(question)
             citation_stack.pop()
         else:
@@ -50,9 +51,9 @@ def parse_questions(text):
 def parse_special_case_question(question):
     # parse special-case questions C.11 q.2, C.17 q.3, C.22 q.3, C.29 q.1, 
     # and C.33 q.3 (de Pen.)
-    m = re.match('\<3 (\d{1,2})\> \<T A\> (.+)', question)
+    m = re.match('\<3 (\d{1,2})\> (\<T A\>) (.+)', question)
     citation_stack.append('q.' + m.group(1))
-    add_to_dictionary('d.a.c.1', m.group(2))
+    add_to_dictionary('d.a.c.1', (m.group(2), m.group(3)))
     citation = ' '.join(citation_stack)
     if citation == 'C.33 q.3': # de Pen.
         tmp_q = citation_stack.pop() # pop 'q.3'
@@ -66,9 +67,9 @@ def parse_de_penitentia(text):
     distinctions = re.findall('(?:\<1 DP\>)(.*?)(?=\<1 DP\>|$)', text)
     for distinction in distinctions:
         distinction = distinction.strip(' ')
-        m = re.match('\<2 (\d)\> \<T A\> (.*?) (?=\<4 1\>)', distinction)
+        m = re.match('\<2 (\d)\> (\<T A\>) (.*?) (?=\<4 1\>)', distinction)
         citation_stack.append('de Pen. D.' + m.group(1))
-        add_to_dictionary('d.a.c.1', m.group(2))
+        add_to_dictionary('d.a.c.1', (m.group(2), m.group(3)))
         parse_canons(distinction)
         citation_stack.pop()
 
@@ -87,7 +88,8 @@ def parse_canons(text):
         canon = canon.strip(' ')
         m = re.match('\<4 (\d{1,3})\>', canon)
         citation_stack.append('c.' + m.group(1))
-        parse_parts(canon)
+        # parse_parts(canon)
+        citation_stack.pop()
 
 def parse_parts(text):
     T_I_flag = False
@@ -114,7 +116,7 @@ def parse_parts(text):
                 citation = ' '.join(citation_stack) + ' d.p.' + canon_number
                 # print('Warning: multiple <T P> tags: ' + citation, file=sys.stderr)
             else:
-                add_to_dictionary(' d.p.' + canon_number, m.group(2))
+                # add_to_dictionary(' d.p.' + canon_number, m.group(2))
                 T_P_flag = True
         if tag == '<T R>':
             if T_R_flag:
@@ -127,14 +129,17 @@ def parse_parts(text):
                 citation = ' '.join(citation_stack) + ' ' + canon_number
                 # print('Warning: multiple <T T> tags: ' + citation, file=sys.stderr)
             else:
-                add_to_dictionary(canon_number, m.group(2))
+                # add_to_dictionary(canon_number, m.group(2))
                 T_T_flag = True
 
-def add_to_dictionary(label, text):
-    citation_stack.append(label)
-    citation = ' '.join(citation_stack)
-    table_of_contents.append(citation)
-    dictionary[citation] = text
+def add_to_dictionary(reference, payload):
+    citation_stack.append(reference)
+    key = ' '.join(citation_stack)
+    table_of_contents.append(key)
+    if key in dictionary:
+        dictionary[key].append(payload)
+    else:
+        dictionary[key] = [payload]
     citation_stack.pop()
 
 def preprocess(text):
