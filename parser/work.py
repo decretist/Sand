201e504e9a732d9f0f5622037a66c933caa03637
Paul Evans <decretist@gmail.com>
2015-02-07 13:18:13 -0500

    Renamed variables in work.py.

--- parser/work.py
+++ parser/work.py
@@ -9,50 +9,59 @@ import re
 import sys
 import parse_decretum
 def main():
-    toc_new = []
-    dictionary_new = {}
-    (canons, dictionary_old) = parse_decretum.parse_decretum()
+    toc_Fr = []
+    dictionary_Fr = {}
+    (canons, intermediate) = parse_decretum.parse_decretum()
     for canon in canons:
-        for part in dictionary_old[canon]:
-            tag = part[0]
-            text = part[1]
+        for node in intermediate[canon]:
+            tag = node[0]
+            text = node[1]
             if tag == '<T A>':
                 if re.search('d.a.c.1', canon):
                     key = canon
                 else: # special case C.16 q.2 d.a.c.8
                     key = re.sub('c.', 'd.a.c.', canon)
-                toc_new.append(key)
-                dictionary_new[key] = text
+                toc_Fr.append(key)
+                dictionary_Fr[key] = text
             if tag == '<T I>':
                 key = canon + ' (i)'
-                if key in dictionary_new:
+                if key in dictionary_Fr:
                     key = canon
-                    text = dictionary_new[key] + ' ' + text
+                    text = dictionary_Fr[key] + ' ' + text
                 else:
-                    toc_new.append(key)
-                dictionary_new[key] = text
+                    toc_Fr.append(key)
+                dictionary_Fr[key] = text
             if tag == '<T P>':
                 key = re.sub('c.', 'd.p.c.', canon)
-                if key in dictionary_new:
-                    text = dictionary_new[key] + ' ' + text
+                if key in dictionary_Fr:
+                    text = dictionary_Fr[key] + ' ' + text
                 else:
-                    toc_new.append(key)
-                dictionary_new[key] = text
+                    toc_Fr.append(key)
+                dictionary_Fr[key] = text
             if tag == '<T Q>':
                 key = canon
-                toc_new.append(key)
-                dictionary_new[key] = text
+                toc_Fr.append(key)
+                dictionary_Fr[key] = text
             if tag == '<T R>':
                 key = canon + ' (r)'
-                toc_new.append(key)
-                dictionary_new[key] = text
+                toc_Fr.append(key)
+                dictionary_Fr[key] = text
             if tag == '<T T>':
                 key = canon
-                if key in dictionary_new:
-                    text = dictionary_new[key] + ' ' + text
+                if key in dictionary_Fr:
+                    text = dictionary_Fr[key] + ' ' + text
                 else:
-                    toc_new.append(key)
-                dictionary_new[key] = text
+                    toc_Fr.append(key)
+                dictionary_Fr[key] = text
+
+    # print(dictionary_Fr['D.32 c.6'])
+    # print(dictionary_Fr['D.54 c.22'])
+    # print(dictionary_Fr['C.4 q.4 c.2'])
+    # print(dictionary_Fr['C.5 q.3 c.1'])
+    # print(dictionary_Fr['C.13 q.2 c.8'])
+    # print(dictionary_Fr['C.16 q.1 c.40'])
+    # print(dictionary_Fr['C.16 q.3 c.16'])
+    # print(dictionary_Fr['C.33 q.2 c.11'])
 
 if __name__ == '__main__':
     main()
