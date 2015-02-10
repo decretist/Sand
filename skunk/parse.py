39c03804b93fb54a0423ef6a1009484f68aafd72
Paul Evans <decretist@gmail.com>
2015-02-10 11:59:25 -0500

    Fully recursive parser for parts 1 and 3.

--- skunk/parse.py
+++ skunk/parse.py
@@ -2,116 +2,58 @@
 #
 # Paul Evans (10evans@cardinalmail.cua.edu)
 # 23 January 2015 -
-# 7 February 2015
+# 9 February 2015
 #
 from __future__ import print_function
 import re
 import sys
-citation_stack = []
-table_of_contents = []
-dictionary = {}
 def parse_all():
+    part_list = []
     file = open('./edF.txt', 'r').read()
     m = re.search('(\<1 D\>.*?)(\<1 C\>.*?)(\<1 DC\>.*?)$', file, re.S)
-    parse_part_1(preprocess(m.group(1)))
-    parse_part_2(preprocess(m.group(2)))
-    parse_part_3(preprocess(m.group(3)))
-    return(table_of_contents, dictionary)
+    part_list.append(parse_part_1(preprocess(m.group(1))))
+    part_list.append(parse_part_3(preprocess(m.group(3))))
+    return(part_list)
 
 # D.1-101
 def parse_part_1(text):
+    distinction_list = []
     distinctions = re.findall('(?:\<1 D\>)(.*?)(?=\<1 D\>|$)', text)
     for distinction in distinctions:
         distinction = distinction.strip(' ')
-        m = re.match('\<2 (\d{1,3})\> (\<T A\>) (.*?) (?=\<4 1\>)', distinction)
-        citation_stack.append('D.' + m.group(1))
-        add_to_dictionary('d.a.c.1', (m.group(2), m.group(3)))
-        parse_canons(distinction)
-        citation_stack.pop()
-
-# C.1-36
-def parse_part_2(text):
-    cases = re.findall('(?:\<1 C\>)(.*?)(?=\<1 C\>|$)', text)
-    for case in cases:
-        case = case.strip(' ')
-        m = re.match('\<2 (\d{1,2})\>(\<T Q\>) (.*?) (?=\<3 1\>)', case)
-        citation_stack.append('C.' + m.group(1))
-        add_to_dictionary('d.init.', (m.group(2), m.group(3)))
-        parse_questions(case)
-        citation_stack.pop()
+        m = re.match('(\<2 \d{1,3}\>) (\<T A\>) (.*?) (\<4 1\>.*?)$', distinction)
+        distinction_list.append((m.group(1), parse_canons(m.group(4))))
+    return(distinction_list)
 
 # de Consecratione
 def parse_part_3(text):
+    distinction_list = []
     distinctions = re.findall('(?:\<1 DC\>)(.*?)(?=\<1 DC\>|$)', text)
     for distinction in distinctions:
         distinction = distinction.strip(' ')
-        m = re.match('\<2 (\d)\> (?=\<4 1\>)', distinction)
-        citation_stack.append('de Cons. D.' + m.group(1))
-        parse_canons(distinction)
-        citation_stack.pop()
+        m = re.match('(\<2 \d\>) (\<4 1\>.*?)$', distinction)
+        distinction_list.append((m.group(1), parse_canons(m.group(2))))
+    return(distinction_list)
 
-def parse_questions(text):
-    questions = re.findall('(\<3 \d{1,2}\>.*?)(?=\<3 \d{1,2}\>|$)', text)
-    for question in questions:
-        question = question.strip(' ')
-        m0 = re.match('\<3 (\d{1,2})\> (\<T A\>) (.*?) (?=\<1 DP\>)', question) # C.33 q.3 (de Pen.)
-        m1 = re.match('\<3 (\d{1,2})\> (\<T A\>) (.*?) (?=\<4 1\>)', question)
-        m2 = re.match('\<3 (\d{1,2})\> (\<T A\>) (.*?)$', question) # C.11 q.2, C.17 q.3, C.22 q.3, C.29 q.1
-        if m0:
-            citation_stack.append('q.' + m0.group(1))
-            add_to_dictionary('d.a.c.1', (m0.group(2), m0.group(3)))
-            tmp_q = citation_stack.pop() # pop 'q.3'
-            tmp_C = citation_stack.pop() # pop 'C.33'
-            parse_de_pen(question)
-            citation_stack.append(tmp_C) # push 'C.33'
-            citation_stack.append(tmp_q) # push 'q.3'
-        elif m1:
-            citation_stack.append('q.' + m1.group(1))
-            add_to_dictionary('d.a.c.1', (m1.group(2), m1.group(3)))
-            parse_canons(question)
-        elif m2:
-            citation_stack.append('q.' + m2.group(1))
-            add_to_dictionary('d.a.c.1', (m2.group(2), m2.group(3)))
-        citation_stack.pop()
-
-# de Penitentia
-def parse_de_pen(text):
-    distinctions = re.findall('(?:\<1 DP\>)(.*?)(?=\<1 DP\>|$)', text)
-    for distinction in distinctions:
-        distinction = distinction.strip(' ')
-        m = re.match('\<2 (\d)\> (\<T A\>) (.*?) (?=\<4 1\>)', distinction)
-        citation_stack.append('de Pen. D.' + m.group(1))
-        add_to_dictionary('d.a.c.1', (m.group(2), m.group(3)))
-        parse_canons(distinction)
-        citation_stack.pop()
-
+# return list of canons
 def parse_canons(text):
+    canon_list = []
     canons = re.findall('(\<4 \d{1,3}\>.*?)(?=\<4 \d{1,3}\>|$)', text)
     for canon in canons:
         canon = canon.strip(' ')
-        m = re.match('\<4 (\d{1,3})\>', canon)
-        citation_stack.append('c.' + m.group(1))
-        parse_tagged_texts(canon)
-        citation_stack.pop()
+        m = re.match('(\<4 \d{1,3}\>) (.*?)$', canon)
+        canon_list.append((m.group(1), parse_nodes(m.group(2))))
+    return(canon_list)
 
-def parse_tagged_texts(text):
-    canon_id = citation_stack.pop() # e.g., 'c.1'
-    parts = re.findall('(\<T [AIPRT]\>.*?)(?=\<T [AIPRT]\>|$)', text)
-    for part in parts:
-        part = part.strip(' ')
-        m = re.match('(\<T [AIPRT]\>) (.+)', part)
-        add_to_dictionary(canon_id, (m.group(1), m.group(2)))
-    citation_stack.append(canon_id) # push canon_id back onto citation stack.
-
-def add_to_dictionary(reference, payload):
-    citation_stack.append(reference)
-    key = ' '.join(citation_stack)
-    if key in dictionary:
-        dictionary[key].append(payload)
-    else:
-        table_of_contents.append(key)
-        dictionary[key] = [payload]
-    citation_stack.pop()
+# return list of leaf nodes (tag-text tuples)
+def parse_nodes(text):
+    node_list = []
+    nodes = re.findall('(\<T [AIPRT]\>.*?)(?=\<T [AIPRT]\>|$)', text)
+    for node in nodes:
+        node =node.strip(' ')
+        m = re.match('(\<T [AIPRT]\>) (.*?)$', node)
+        node_list.append((m.group(1), m.group(2)))
+    return(node_list)
 
 def preprocess(text):
     text = re.sub(re.compile('\-.*?\+', re.S), '', text) # remove comments
--- skunk/test.py
+++ skunk/test.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 #
 # Paul Evans (10evans@cardinalmail.cua.edu)
-# 8 February 2015
+# 9 February 2015
 #
 from __future__ import print_function
 import re
 import sys
 import parse
 def main():
-    (toc_Fr, dictionary_Fr) = parse.parse_all()
-    for key in toc_Fr:
-        print(key + ': ' + str(dictionary_Fr[key]))
+    parse.parse_all()
 
 if __name__ == '__main__':
     main()
