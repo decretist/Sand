50369939c683c9a43ce45d0910410941d0304920
Paul Evans <decretist@gmail.com>
2015-02-17 10:53:20 -0500

    Fixed bug in wc_canons.py: single-line comments have to stripped out before multi-line comments, or greedy matching will remove too much text.

--- count/wc_canons.py
+++ count/wc_canons.py
@@ -16,18 +16,17 @@ def main():
             '\<2 \d{1,3}\>|'    # or number of major division,
             '\<3 \d{1,2}\>|'    # or number of question,
             '\<4 \d{1,3}\>|'    # or number of canon,
-            '\<P 1\>|'          # or Palea,
             '\<T [AIPT]\>|'     # or dictum or inscription or text tag,
             '$'                 # or end of file.
         ')', file, re.S)        # re.S (re.DOTALL) makes '.' special character match any character including newline.
     # print('expected 4394 canons, found ' + str(len(canons)) + ' canons', file=sys.stderr)
     for canon in canons:
         canon = re.sub('\<S \d{1,4}\>\<L 1\> \-\d{1,4}\+', '', canon) # remove page and line number tags.
-        canon = re.sub('\<P 1\> \-\[PALEA\.\+', '', canon) # remove Palea tag.
-        canon = re.sub('\<P 0\>', '', canon) # remove Palea tag.
+        canon = re.sub('\<P 1\>', '', canon) # remove Palea tag.
         canon = re.sub('\<T I\>', '', canon) # remove inscription tag.
-        canon = re.sub(re.compile('\-\[.*?\]\+', re.S), '', canon)
+        canon = re.sub('\<P 0\>', '', canon) # remove Palea tag.
         canon = re.sub('\-.*?\+', '', canon)
+        canon = re.sub(re.compile('\-\[.*?\]\+', re.S), '', canon)
         canon = re.sub('\s+', ' ', canon)
         canon = re.sub('^\s+', '', canon) # remove leading whitespace characters
         canon = re.sub('\s+$', '', canon) # remove trailing whitespace characters
